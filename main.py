2cf6e6a38790ebc8a075d1f9fc83f9a1b2d9afad
SidLabs <98277470+SidLabs-Online@users.noreply.github.com>
2022-08-31 07:35:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# What are variables in programming ?
+# They are likes boxes where you can keep stuff and then use 'em when you need'
+
+# What kind of things can you place in these boxes ?
+# There are x 4 primitive data types that we can store in these variables or constants
+# "=" sign is known as assignment operator 
+
+# This is called an interger
+var1 = 12 
+
+#This is a called float
+var2 = 23.45
+
+# This is called a String
+var3 = "Lancelot"
+
+# This is called a Boolean
+var4 = False
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+myList = [12,13,14,15]
+
+myList.append(24)
+
+print(myList)
